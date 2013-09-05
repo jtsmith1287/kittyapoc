df8b6feed7fc8c30f1108ce200cec0771e437066
jtsmith1287@gmail.com <>
2013-09-05 04:26:46 +0000

    pushing to local

--- main.py
+++ main.py
@@ -1,49 +1,76 @@
 import webapp2
 import random
+import traceback
 
-def getText(_file):
+
+def getHtml(_file):
   with open(_file, "r") as f: 
     return f.read().replace("\n", "")
 
 
-HTML = {"main_page": getText("main_page.html"),
-        "adventure": getText("adventure.html"),
-        "kennel": getText("kennel.html")}
-
+HTML = {"main_page": getHtml("main_page.html"),
+        "adventure": getHtml("adventure.html"),
+        "kennel": getHtml("kennel.html")}
 
 
 class Kat:
   def __init__(self):
     self.name = random.choice(["Kitty", "Kitten", "Kat"])
+    self.level = 1
+    self.xp = {"Current XP": 0, 
+               "XP to Level": 10}
+    self.combat = {"Attack": 1, 
+                   "Defense": 1}
+  
+  def gatherStats(self):
+    stats = []
+    stats.append("<b>%s</b>:<br>" %(self.name))
+    return "".join(stats)
 
 
 class Player:
 
   def __init__(self):
-    self.cats = [Kat()]
+    self.cats = [Kat() for i in xrange(5)]
 
 player = Player()
+
 
 class MainPage(webapp2.RequestHandler):
 
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.write(HTML["main_page"])
+    try:
+      self.response.write(HTML["main_page"])
+    except Exception:
+      self.response.write(traceback.format_exc())
+
 
 class Adventure(webapp2.RequestHandler):
 
   def post(self):
-    self.response.write(HTML["adventure"])
+    try:
+      self.response.write(HTML["adventure"])
+    except Exception:
+      self.response.write(traceback.format_exc())
 
 
 class Kennel(webapp2.RequestHandler):
 
   def getSubs(self):
-    return player.__dict__
+    
+    sub = {"cats": None}
+    p_dict = player.__dict__.copy()
+    sub["cats"] = "".join([cat.gatherStats()) for cat in p_dict["cats"]])
+    return sub
+    
 
   def post(self):
     sub = self.getSubs()
-    self.response.write(HTML["kennel"] %(sub))
+    try:
+      self.response.write(HTML["kennel"] %(sub))
+    except Exception:
+      self.response.write(traceback.format_exc())
 
 
 handlers = [('/', MainPage),
